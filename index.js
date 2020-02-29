f78fec336aac71ed18ec73048634f3e3105fd1c6
Matthew Cheney <matt1cheney@gmail.com>
2020-02-29 12:22:01 -0500

    added pseudocode to index.js

---
+++ index.js
@@ -0,0 +1,24 @@
+// we need to make a word guess game!
+
+// we will need to loop through possible words to guess.
+//   make a js file to list possible words.
+
+
+// we will need to use inquirer to guess a letter.
+
+
+// we need to set a remaining guesses count.
+
+// if a letter is wrong, we need to tell them its wrong and 
+// subtract remaining guesses.
+
+// if a letter is correct we need to show that they were correct and then show
+// all of the instances the letter was used.
+
+// once they have guessed all the letters we need to run a winner
+// prompt and then end the game.
+
+// if they run out of guesses we need to run a loser prompt and end the game
+
+
+// ask if they want to play again!
